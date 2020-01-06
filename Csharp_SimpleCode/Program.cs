1dc84377629331ddad829d757b5821944f1a6b1f
Viacheslav <v.haliullin@pixonic.com>
2020-01-07 02:58:23 +0300

    init

---
+++ Csharp_SimpleCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_SimpleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
